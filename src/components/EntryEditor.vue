2c99cbb3d36cbad5a5e1c511e89e143ef7c7336b
RustambekJavascript <rustambekeshboriyev@gmail.com>
2022-10-02 11:29:48 +0500

    completed

---
+++ src/components/EntryEditor.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+import EmojiField from "@/components/EmojiField.vue";
+import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from '../types/Emoji';
+
+import { ref, computed, onUnmounted, inject } from "vue";
+import type Entry from "@/types/Entry";
+import { userInjectionKey } from "@/InjectionKeys";
+
+const body = ref("");
+
+const emoji = ref<Emoji | null>(null);
+
+const charCount = computed(() => body.value.length);
+
+const maxChars = 280;
+
+const user = inject(userInjectionKey)
+const textarea = ref<HTMLTextAreaElement | null>(null);
+
+onUnmounted(() => textarea.value?.focus())
+
+const emit = defineEmits<{
+  (e: "@create", entry: Entry): void;
+}>();
+
+const handleSubmit = () => {
+  emit("@create", {
+    body: body.value,
+    emoji: emoji.value,
+    createdAt: new Date(),
+    userId: 1,
+    id: Math.random(),
+  })
+
+  body.value = "";
+  emoji.value = null;
+}
+
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+
+  if (textarea.value.length <= maxChars) {
+    body.value = textarea.value;
+  } else {
+    body.value = textarea.value = textarea.value.substring(0, 280);
+  }
+}
+
+</script>
+<template>
+  <form class="entry-form" @submit.prevent="handleSubmit">
+    <textarea ref="textarea" :value="body" @keyup="handleTextInput"
+      :placeholder="`New Journal Entry for ${user?.username || 'anonymous'}`"></textarea>
+    <EmojiField v-model="emoji" />
+    <div class="entry-form-footer">
+      <span>{{charCount}} / {{maxChars}}</span>
+      <button>Remember
+        <ArrowCircleRight width="20" />
+      </button>
+    </div>
+  </form>
+</template>
---
+++ src/types/Emoji.ts
@@ -0,0 +1,17 @@
+type Emoji =
+  | "confused"
+  | "coll"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heart-eyes"
+  | "laugh"
+  | "sad"
+  | "chocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emoji;
